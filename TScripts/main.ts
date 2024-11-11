a1ddb1c69ae1459b8e359a9cdbfc8dbb07d70101
whosedenny <90749476+whosedenny@users.noreply.github.com>
2024-11-11 17:01:50 +0200

    add validation system

--- TScripts/main.ts
+++ TScripts/main.ts
@@ -40,3 +40,72 @@ type AccessControl<T extends BaseContent> = {
         [P in keyof T]?: Permission;
     };
 };
+
+type Validator<T> = {
+    validate: (data: T) => ValidationResult;
+}
+
+type ValidationResult = {
+    isValid: boolean;
+    errors?: string[];
+};
+
+const articleValidator: Validator<Articel> = {
+    validate(data: Articel): ValidationResult {
+        const errors: string[] = [];
+        if(!data.description){
+            errors.push("Введіть опис");
+        }
+        if (!data.title || data.title.length < 1) {
+            errors.push("Назва занад-то коротка. Введіть мінімум 2 символи");
+        }
+        if (!data.articelText) {
+            errors.push("Текст не може бути порожнім");
+        }
+        return { isValid: errors.length === 0, errors };
+    }
+};
+
+
+const productValidator: Validator<Product> = {
+    validate(data: Product): ValidationResult {
+        const errors: string[] = [];
+        if (!data.description) {
+            errors.push("Введіть опис");
+        }
+        if (!data.name || data.name.length < 2) {
+            errors.push("Назва занад-то коротка. Введіть мінімум 2 символи");
+        }
+        if (!data.type) {
+            errors.push("Введіть назву типу");
+        }
+        if (data.price <= 0) {
+            errors.push("Ціна не може бути від'ємною");
+        }
+        return { isValid: errors.length === 0, errors };
+    }
+};
+
+class CompositeValidator<T> implements Validator<T> {
+    private validators: Validator<T>[];
+
+    constructor(...validators: Validator<T>[]) {
+        this.validators = validators;
+    }
+
+    validate(data: T): ValidationResult {
+        const errors: string[] = [];
+        let isValid = true;
+
+        for (const validator of this.validators) {
+            const result = validator.validate(data);
+            if (!result.isValid) {
+                isValid = false;
+                if (result.errors) {
+                    errors.push(...result.errors);
+                }
+            }
+        }
+        return { isValid, errors };
+    }
+}
